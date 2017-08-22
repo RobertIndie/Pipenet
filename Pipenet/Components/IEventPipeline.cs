e266a7b74e15ef4b8b772fd16732df460bc23725
Robert <RobertIndie@outlook.com>
2017-08-22 20:08:26 +0800

    更新代码架构

---
+++ Pipenet/Components/IEventPipeline.cs
@@ -0,0 +1,57 @@
+﻿using Pipenet.Transport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pipenet.Components
+{
+    public interface IAddEvent
+    {
+        /// <summary>
+        /// 添加没有返回值的事件
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="method"></param>
+        void AddEvent(string name, Action<ITransport, object[]> method);
+        /// <summary>
+        /// 添加事件
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="method"></param>
+        void AddReturnEvent(string name, Func<ITransport, object[], object> method);
+    }
+    public interface IEventPipeline : IConnectState, IAddEvent, IConnectEvent
+    {
+        /// <summary>
+        /// 管道连接
+        /// </summary>
+        void Connect();
+        /// <summary>
+        /// 触发事件
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="parameters"></param>
+        /// <param name="isReturn"></param>
+        /// <returns></returns>
+        object Invoke(string name, object[] parameters, bool isReturn = false);
+    }
+    public interface IMultiTransport : IAddEvent, IConnectEvent
+    {
+        event Action<ITransport> onSubTransportConnect;
+        event Action<ITransport> onSubTransportDisconnect;
+        List<ITransport> subTransportPool
+        {
+            get;
+        }
+        void Connect();
+        void Invoke(ITransport transport, string name, object[] parameters, bool isReturn = false);
+        bool IsListenning
+        {
+            get;
+        }
+    }
+    public interface IConnectEvent
+    {
+        event Action<ITransport> onConnect;
+    }
+}
--- Pipenet/Components/Pipeline.cs
+++ Pipenet/Components/Pipeline.cs
@@ -6,55 +6,6 @@ using System.Threading;
 
 namespace Pipenet.Components
 {
-    public interface IAddEvent
-    {
-        /// <summary>
-        /// 添加没有返回值的事件
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="method"></param>
-        void AddEvent(string name, Action<ITransport, object[]> method);
-        /// <summary>
-        /// 添加事件
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="method"></param>
-        void AddReturnEvent(string name, Func<ITransport, object[], object> method);
-    }
-    public interface IEventPipeline:IConnectState,IAddEvent,IConnectEvent
-    {
-        /// <summary>
-        /// 管道连接
-        /// </summary>
-        void Connect();
-        /// <summary>
-        /// 触发事件
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="parameters"></param>
-        /// <param name="isReturn"></param>
-        /// <returns></returns>
-        object Invoke(string name, object[] parameters,bool isReturn = false);
-    }
-    public interface IMultiTransport:IAddEvent,IConnectEvent
-    {
-        event Action<ITransport> onSubTransportConnect;
-        event Action<ITransport> onSubTransportDisconnect;
-        List<ITransport> subTransportPool
-        {
-            get;
-        }
-        void Connect();
-        void Invoke(ITransport transport, string name, object[] parameters, bool isReturn = false);
-        bool IsListenning
-        {
-            get;
-        }
-    }
-    public interface IConnectEvent
-    {
-        event Action<ITransport> onConnect;
-    }
     public class PipelineSettings
     {
         public enum ConnectionType
@@ -95,13 +46,8 @@ namespace Pipenet.Components
         PipelineSettings settings;
         ITransport transport;
         internal List<ITransport> _subTransportPool = new List<ITransport>();
-        public List<ITransport> subTransportPool
-        {
-            get
-            {
-                return _subTransportPool;
-            }
-        }
+        public List<ITransport> subTransportPool { get => _subTransportPool; }
+        #region SocketTransport
         public event Action<ITransport> onSubTransportConnect;
         public event Action<ITransport> onSubTransportDisconnect;
         public event Action<ITransport> onConnect;
@@ -109,6 +55,7 @@ namespace Pipenet.Components
         internal void invokeSubTransportConnect(ITransport subTransport) => onSubTransportConnect(subTransport);
         internal void invokeSubTransportDisconnect(ITransport subTransport) => onSubTransportDisconnect(subTransport);
         internal void invokeOnConnect(ITransport transport) => onConnect(transport);
+        #endregion
         public Pipeline(PipelineSettings settings)
         {
             this.settings = settings;
